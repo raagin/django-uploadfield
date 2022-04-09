4730d803c45973940b209aeaba15a92e78b278fc
Lapshinov Yury <y.raagin@gmail.com>
2022-04-09 07:54:09 +0300

    keep files on delete instance

--- uploadfield/conf.py
+++ uploadfield/conf.py
@@ -2,3 +2,4 @@ from django.conf import settings
 
 TEMP_DIR = getattr(settings, "UPLOADFIELD_TEMP_DIR", 'tmp/')
 THUMBNAIL = getattr(settings, "UPLOADFIELD_THUMBNAIL", 'admin_thumbnail')
+KEEP_FILES_ON_DELETE = getattr(settings, "UPLOADFIELD_KEEP_FILES_ON_DELETE", False)
