9804192303c70e16420e1be9722f143728455019
Lapshinov Yury <y.raagin@gmail.com>
2022-11-17 23:57:54 +0300

    fix import ugettext_lazy

--- uploadfield/fields.py
+++ uploadfield/fields.py
@@ -1,6 +1,6 @@
 import os
 import json
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.template.loader import render_to_string
 from django.db.models.fields import CharField
 from django.contrib.admin.options import FORMFIELD_FOR_DBFIELD_DEFAULTS
